d108099a1185af323d097ea51eb787c75a6a43ee
souravsahums <114210536+souravsahums@users.noreply.github.com>
2025-05-05 20:49:30 +0530

    Update document_tool.py

--- tools/document_tool.py
+++ tools/document_tool.py
@@ -1,8 +1,8 @@
 from langchain.tools import Tool
-from utils.vector_utils import get_vectorstore
+from utils.vector_utils import get_vectore_store
 
 def document_search_tool():
-    vectorstore = get_vectorstore()
+    vectorstore = get_vectore_store()
     retriever = vectorstore.as_retriever(search_type="mmr", search_kwargs={"k": 5})
     return Tool.from_function(
         func=lambda q: retriever.get_relevant_documents(q),
